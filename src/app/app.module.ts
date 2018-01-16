858e52bbee8022c507ac52a4a4e274c44f3c2560
rakesh <rakathegreat71@gmail.com>
2018-01-17 00:54:55 +0530

    front is connecting to back end and registering user and i have checked register users are present in mongodb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +8,13 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ValidateService} from './validate.service';
+import { FlashMessageModule}  from "angular-flash-message";
+import {AuthService} from "./auth.service";
+import {Http,Headers}from "@angular/http"
+import {HttpModule} from "@angular/http";
+
+
 
 const appRoutes: Routes = [
   { 
@@ -42,12 +49,15 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FlashMessageModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  providers: [],
+  providers: [ValidateService,AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService} from "../validate.service";
+import { AuthService} from "../auth.service";
+import { FlashMessage} from "angular-flash-message";
+import{ Router}  from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+	selector: 'app-register',
+	templateUrl: './register.component.html',
+	styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+	name:String;
+	username:String;
+	email:String;
+	password:String;
+	constructor(
+		private validation:ValidateService, 
+		private flashMessage:FlashMessage,
+		private authService:AuthService,
+		private router:Router
+		) { }
 
-  constructor() { }
+	ngOnInit() {
+	}
 
-  ngOnInit() {
-  }
+	onRegisterSubmit(){
+		const user = {
+			name:this.name,
+			username:this.username,
+			email:this.email,
+			password:this.password
+		}
+
+		if (this.validation.validateRegister(user)) {
+			this.flashMessage.danger("fill all the fields");
+			// false will hold the page unless it will reload
+			return false;
+		}
+
+		if(!this.validation.validateEmail(user.email)){
+			console.log("email validation is working");
+			this.flashMessage.danger("enter a valid email address");
+			return false;
+			
+		}
+
+		this.authService.registerUser(user).subscribe(data => {
+			if (data.success) {
+				this.flashMessage.success('you are registered and now you can login');
+				this.router.navigate(['/']);
+			}else{
+				this.flashMessage.danger('something is wrong and please re-try');
+				this.router.navigate(['/register']);
+			}
+		})
+	}
+
 
 }
