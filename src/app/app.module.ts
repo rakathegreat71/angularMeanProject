17c29c837c8c7da771b595f9d4f9942d187c54d6
rakesh <rakathegreat71@gmail.com>
2018-01-16 14:49:32 +0530

    Basic navbar and routes setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [
+  { 
+    path: '', 
+    component:HomeComponent 
+  },
+  { 
+    path: 'register', 
+    component: RegisterComponent 
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { 
+    path: 'dashboard',
+    component:DashboardComponent
+  }
+];
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
